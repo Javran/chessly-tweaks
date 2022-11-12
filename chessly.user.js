711a9b23f21580ff9e53ce397e812d8859e00ad0
Javran Cheng <Javran.C@gmail.com>
2022-11-12 15:05:54 -0800

    a bit of cleanup.

--- chessly.user.js
+++ chessly.user.js
@@ -3,20 +3,20 @@
 // @description Some random Chessly tweaks.
 // @homepage https://github.com/Javran/chessly-tweaks
 // @namespace javran.github.io
-// @version 0.1
+// @version 0.2
 // @author Javran Cheng
 // @match https://chessly.com/*
 // @grant none
 // @require https://code.jquery.com/jquery-3.6.1.min.js
 // ==/UserScript==
 
-const $ = window.$;
+(() => {
+  const {$} = window
 
-$(() => {
   document.addEventListener('keydown', e => {
     const {href} = window.location
     if (href && href.endsWith('drill-shuffle') && e.key === 'f') {
-      $('button.PrimaryActionButton_button__MrAca').click();
+      $('button.PrimaryActionButton_button__MrAca').click()
     }
-  });
-});
+  })
+})()
